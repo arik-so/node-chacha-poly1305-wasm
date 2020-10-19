56653c66d7f17dc2883dd29024b242c0728d29c6
Arik Sosman <git@arik.io>
2020-10-19 15:39:52 -0700

    add node test

---
+++ native/wasm-test/node_test.js
@@ -0,0 +1,22 @@
+// import wasmStuff from './chacha_poly1305_rust.js';
+const fs= require('fs');
+const wasmStuff = require('../bin/chacha_poly1305_rust.js');
+
+const wasm = fs.readFileSync(__dirname + '/../bin/chacha_poly1305_rust_bg.wasm');
+const base64 = wasm.toString('base64');
+
+fs.writeFileSync(__dirname + '/base64.js', `const wasm = '${wasm.toString('base64')}';\n\nmodule.exports.wasmString = wasm;`);
+// console.log(base64);
+
+const callback = (a, b) => {
+	return a + b;
+}
+
+const initMessage = wasmStuff.perform_complicated_calculation();
+// const initMessage = wasmStuff.create_init_message();
+console.dir(initMessage);
+
+// const result = wasmStuff.call_callback(callback);
+// console.log(result);
+
+
