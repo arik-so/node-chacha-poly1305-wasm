14d1e9c71653f87231eb916cfebf6acc6cc0da11
Arik Sosman <git@arik.io>
2020-10-19 16:11:10 -0700

    convert to base64 first, then run test

--- native/wasm-test/node_test.js
+++ native/wasm-test/node_test.js
@@ -1,12 +1,13 @@
 // import wasmStuff from './chacha_poly1305_rust.js';
 const fs= require('fs');
-const wasmStuff = require('../bin/chacha_poly1305_rust.js');
-
 const wasm = fs.readFileSync(__dirname + '/../bin/chacha_poly1305_rust_bg.wasm');
 const base64 = wasm.toString('base64');
-
 fs.writeFileSync(__dirname + '/base64.js', `const wasm = '${wasm.toString('base64')}';\n\nmodule.exports.wasmString = wasm;`);
-// console.log(base64);
+console.log(base64);
+
+
+
+const wasmStuff = require('../bin/chacha_poly1305_rust.js');
 
 const callback = (a, b) => {
 	return a + b;
